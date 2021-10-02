323ee391d843045eec062276706a87a6cc4a82e2
Robson Carvalho <robscarvalho8@gmail.com>
2021-10-02 13:41:26 -0300

    Sliding Token (/refresh-token-2/)

--- config/settings/base.py
+++ config/settings/base.py
@@ -290,6 +290,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
         "rest_framework.authentication.TokenAuthentication",
+        "rest_framework_simplejwt.tokens.SlidingToken",
     ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
 }
