71785e494138a236f87ded3a3659541b0543a2b3
robscarvalho8 <robscarvalho8@gmail.com>
2021-12-06 10:59:15 -0300

    bsides.dev

--- backend/contrib/sites/migrations/0003_set_site_domain_and_name.py
+++ backend/contrib/sites/migrations/0003_set_site_domain_and_name.py
@@ -13,7 +13,7 @@ def update_site_forward(apps, schema_editor):
     Site.objects.update_or_create(
         id=settings.SITE_ID,
         defaults={
-            "domain": "virtualsoft.dev.br",
+            "domain": "bsides.dev",
             "name": "OPE2 Divina Hamburgueria",
         },
     )
@@ -23,7 +23,7 @@ def update_site_backward(apps, schema_editor):
     """Revert site domain and name to default."""
     Site = apps.get_model("sites", "Site")
     Site.objects.update_or_create(
-        id=settings.SITE_ID, defaults={"domain": "virtualsoft.dev.br", "name": "Virtualsoft"}
+        id=settings.SITE_ID, defaults={"domain": "bsides.dev", "name": "Virtualsoft"}
     )
 
 
